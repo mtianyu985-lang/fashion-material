80b3d1e8bf1d72a6e573cb3c363ffc0b46d16924
mtianyu985-lang <mtianyu985@gmail.com>
2026-09-18 22:06:37 +0800

    fix: 修改start_url为/-/并升级缓存版本v4

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    服装设计素材库 · Service Worker
    ============================================ */
 
-const CACHE_NAME = 'fashion-material-v3';
+const CACHE_NAME = 'fashion-material-v4';
 const STATIC_ASSETS = [
   './',
   './index.html',
