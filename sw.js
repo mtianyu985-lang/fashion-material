ab7b57431fe0ede8870836de530e3e0fb42d7e59
mtianyu985-lang <mtianyu985@gmail.com>
2026-09-19 10:36:47 +0800

    v1.4.8 修复: manifest start_url改为/fashion-material/,SW缓存升v7,主屏幕图标不再404

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    服装设计素材库 · Service Worker
    ============================================ */
 
-const CACHE_NAME = 'fashion-material-v6';
+const CACHE_NAME = 'fashion-material-v7';
 const STATIC_ASSETS = [
   './',
   './index.html',
